d59b0c056a167d377fae707ba73d783f48078bf1
Eduard Korchmar <korchmar@ohdsi.org>
2026-04-15 21:01:45 +0200

    chore: extract SNOMED primitives into own module

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import pstats
 import re
 import sys
 import unittest
-from dataclasses import dataclass
 from utils.constants import (
     AVAILABLE_PROMPTERS,
     CSV_SEPARATORS,
@@ -65,7 +64,6 @@ from utils.types import (
     EscapeHatch,
     SCTDescription,
     JsonPrimitive,
-    SCGExpression,
     Json,
 )
 
@@ -74,6 +72,14 @@ from prompter import (
     OpenAIAzurePrompter,
     OpenAIPrompter,
     HumanPrompter,
+)
+from models import (
+    AttributeRelationship,
+    Concept,
+    MRCMDomainRefsetEntry,
+    AttributeConstraints,
+    SemanticPortrait,
+    WrappedResult,
 )
 
 # Optional imports
@@ -169,301 +175,6 @@ class IOParametersWidget(QtWidgets.QWidget):
 
 LOGGER.info(f"Parameters loaded: {json.dumps(PARAMS.model_dump(), indent=2)}")
 LOGGER.setLevel(PARAMS.log.logging_level)
-
-
-## Dataclasses
-### SNOMED modelling
-@dataclass(frozen=True, slots=True)
-class AttributeRelationship:
-    """Represents a SNOMED CT attribute-value pair relationship."""
-
-    attribute: SCTID
-    value: SCTID
-
-
-@dataclass(frozen=True, slots=True)
-class AttributeGroup:
-    """Represents a SNOMED CT attribute group."""
-
-    relationships: frozenset[AttributeRelationship]
-
-
-@dataclass(frozen=True, slots=True)
-class Concept:
-    """Represents a SNOMED CT concept."""
-
-    sctid: SCTID
-    pt: SCTDescription
-    fsn: SCTDescription
-    groups: frozenset[AttributeGroup]
-    ungrouped: frozenset[AttributeRelationship]
-    defined: bool
-
-    @classmethod
-    def from_rela_json(cls, relationship_data: list[dict]):
-        # Get concept proper info
-        # Everyone is expected to have at least an 'Is a' relationship
-        # Except root, but encountering it is an error
-        if len(relationship_data) == 0:
-            raise ValueError("No relationships found; could it be a root?")
-
-        specimen = relationship_data[0]["source"]
-        sctid = SCTID(specimen["conceptId"])
-        pt = SCTDescription(specimen["pt"]["term"])
-        fsn = SCTDescription(specimen["fsn"]["term"])
-        defined = specimen["definitionStatus"] == "FULLY_DEFINED"
-
-        # Get relationships
-        groups: dict[int, set[AttributeRelationship]] = {}
-        ungrouped: set[AttributeRelationship] = set()
-        for relationship in relationship_data:
-            attribute = SCTID(relationship["type"]["conceptId"])
-            # Skip 'Is a' relationships here
-            if attribute == IS_A:
-                continue
-            value = SCTID(relationship["target"]["conceptId"])
-            group = relationship["groupId"]
-            rel = AttributeRelationship(attribute, value)
-            if group == 0:
-                ungrouped.add(rel)
-            else:
-                groups.setdefault(group, set()).add(rel)
-
-        return cls(
-            sctid=sctid,
-            pt=pt,
-            fsn=fsn,
-            groups=frozenset(
-                AttributeGroup(frozenset(relationships))
-                for relationships in groups.values()
-            ),
-            ungrouped=frozenset(ungrouped),
-            defined=defined,
-        )
-
-    @classmethod
-    def from_json(cls, json_data: dict):
-        groups: dict[int, set[AttributeRelationship]] = {}
-        ungrouped: set[AttributeRelationship] = set()
-        for relationship in json_data["relationships"]:
-            if not relationship["active"]:
-                continue
-
-            attribute = SCTID(relationship["type"]["conceptId"])
-            # Skip 'Is a' relationships here
-            if attribute == IS_A:
-                continue
-            value = SCTID(relationship["target"]["conceptId"])
-            rel = AttributeRelationship(attribute, value)
-            if (group := relationship["groupId"]) == 0:
-                ungrouped.add(rel)
-            else:
-                groups.setdefault(group, set()).add(rel)
-
-        match json_data["definitionStatus"]:
-            case "FULLY_DEFINED":
-                defined = True
-            case "PRIMITIVE":
-                defined = False
-            case _:
-                raise (ValueError("Unknown definition status"))
-
-        return cls(
-            sctid=SCTID(json_data["conceptId"]),
-            pt=SCTDescription(json_data["pt"]["term"]),
-            fsn=SCTDescription(json_data["fsn"]["term"]),
-            groups=frozenset(
-                AttributeGroup(frozenset(relationships))
-                for relationships in groups.values()
-            ),
-            ungrouped=frozenset(ungrouped),
-            defined=defined,
-        )
-
-
-@dataclass(frozen=True, slots=True)
-class MRCMDomainRefsetEntry:
-    """\
-Represents an entry in the MRCM domain reference set.
-
-Used mainly to obtain an entry ancestor anchor for a semantic portrait. Contains
-more useful information for domain modelling, but it is not yet well explored.
-"""
-
-    # Concept properties
-    sctid: SCTID
-    term: SCTDescription
-
-    # Additional fields
-    domain_constraint: ECLExpression
-    guide_link: Url  # For eventual RAG connection
-    # Currently uses unparseable extension of ECL grammar,
-    # but one day we will use it
-    domain_template: ECLExpression
-    parent_domain: ECLExpression | None = None
-    proximal_primitive_refinement: ECLExpression | None = None
-
-    @classmethod
-    def from_json(cls, json_data: dict):
-        af = json_data["additionalFields"]
-        dom = af.get("parentDomain")
-        prf = af.get("proximalPrimitiveRefinement")
-
-        return cls(
-            sctid=SCTID(json_data["referencedComponent"]["conceptId"]),
-            term=SCTDescription(json_data["referencedComponent"]["pt"]["term"]),
-            domain_template=ECLExpression(
-                # Use precoordination: stricter
-                af["domainTemplateForPrecoordination"]
-            ),
-            domain_constraint=ECLExpression(af["domainConstraint"]),
-            guide_link=Url(af["guideURL"]),
-            parent_domain=ECLExpression(dom) if dom else None,
-            proximal_primitive_refinement=ECLExpression(prf) if prf else None,
-        )
-
-
-# Solving for presence vs. absence of attributes is way, way easier than solving
-# quantitative problems. For now, Cardinality will likely be ignored.
-@dataclass(frozen=True, slots=True)
-class Cardinality:
-    """\
-Represents a cardinality of an attribute in a group or a definition.
-"""
-
-    min: int
-    max: None | int
-
-
-@dataclass(frozen=True, slots=True)
-class AttributeDomain:
-    """\
-Represents a domain of applications of an attribute.
-"""
-
-    sctid: SCTID
-    pt: SCTDescription
-    domain_id: SCTID
-    grouped: bool
-    cardinality: Cardinality
-    in_group_cardinality: Cardinality
-
-
-@dataclass(frozen=True, slots=True)
-class AttributeRange:
-    """\
-Represents a range of values of an attribute.
-"""
-
-    sctid: SCTID
-    pt: SCTDescription
-    range_constraint: ECLExpression
-    attribute_rule: ECLExpression
-    contentType: str
-
-
-@dataclass(frozen=True, slots=True)
-class AttributeConstraints:
-    """\
-Represents information about an attribute obtained from a known set of parent
-concepts.
-"""
-
-    sctid: SCTID
-    pt: SCTDescription
-    attribute_domain: Iterable[AttributeDomain]
-    # May actually not be required
-    # because /mrcm/{branch}/attribute-values/ exists
-    attribute_range: Iterable[AttributeRange]
-
-    @classmethod
-    def from_json(cls, json_data: dict):
-        return cls(
-            sctid=SCTID(json_data["conceptId"]),
-            pt=SCTDescription(json_data["pt"]["term"]),
-            attribute_domain=[
-                AttributeDomain(
-                    sctid=SCTID(json_data["conceptId"]),
-                    pt=SCTDescription(json_data["pt"]["term"]),
-                    domain_id=SCTID(ad["domainId"]),
-                    grouped=ad["grouped"],
-                    cardinality=Cardinality(
-                        ad["attributeCardinality"]["min"],
-                        ad["attributeCardinality"].get("max"),
-                    ),
-                    in_group_cardinality=Cardinality(
-                        ad["attributeInGroupCardinality"]["min"],
-                        ad["attributeInGroupCardinality"].get("max"),
-                    ),
-                )
-                for ad in json_data["attributeDomain"]
-            ],
-            attribute_range=[
-                AttributeRange(
-                    sctid=SCTID(json_data["conceptId"]),
-                    pt=SCTDescription(json_data["pt"]["term"]),
-                    range_constraint=ECLExpression(ar["rangeConstraint"]),
-                    attribute_rule=ECLExpression(ar["attributeRule"]),
-                    contentType=ar["contentType"],
-                )
-                for ar in json_data["attributeRange"]
-            ],
-        )
-
-
-### Mutable portrait
-@dataclass
-class SemanticPortrait:
-    """\
-Represents an interactively built semantic portrait of a source concept."""
-
-    def __init__(
-        self,
-        term: str,
-        context: Iterable[str] | None = None,
-        metadata: frozendict[str, JsonPrimitive] = frozendict(),
-    ) -> None:
-        self.source_term: str = term
-        self.context: Iterable[str] | None = context
-        self.ancestor_anchors: set[SCTID] = set()
-
-        self.unchecked_attributes: set[SCTID] = set()
-        self.attributes: dict[SCTID, SCTID] = {}
-        self.rejected_attributes: set[SCTID] = set()
-        self.rejected_supertypes: set[SCTID] = set()
-
-        self.relevant_constraints: dict[SCTID, AttributeConstraints] = {}
-
-        self.metadata: frozendict[str, JsonPrimitive] = metadata
-
-    def to_scg(self: SemanticPortrait) -> SCGExpression:
-        """\
-Convert a SemanticPortrait to a SNOMED CT Post-Coordinated Expression
-"""
-        # Always Subtype
-        prefix = "<<<"
-        focus_concepts = "+".join(str(a) for a in self.ancestor_anchors)
-        attributes = ",".join(f"{a}={v}" for a, v in self.attributes.items())
-        return SCGExpression(
-            prefix
-            + (focus_concepts or str(ROOT_CONCEPT))
-            + ":" * bool(attributes)
-            + attributes
-        )
-
-
-class WrappedResult:
-    """\
-Represents a completed semantic portrait with additional metadata.
-"""
-
-    def __init__(
-        self,
-        portrait: SemanticPortrait,
-        name_map: Mapping[SCTID, SCTDescription],
-    ) -> None:
-        self.portrait: SemanticPortrait = portrait
-        self.name_map: Mapping[SCTID, SCTDescription] = name_map
 
 
 ## Hacked Httpx client
